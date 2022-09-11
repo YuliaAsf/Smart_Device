83bfc104bdccc9feec6b94d66dd08b40de6e3184
Yulia Asfanderova <nadberejenka@yandex.ru>
2022-09-11 16:54:01 +0300

    about section

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,74 @@
+// Кнопка скрытия/показа контента в разделе "О компании"
+
+const button = document.querySelector('.about button');
+const contentBlocks = document.querySelectorAll('.about p');
+const contentElement = document.querySelector('.about span');
+
+// const mediaQueryTablet = window.matchMedia('(min-width: 768px)');
+const mediaQueryMobile = window.matchMedia('(max-width: 767.9px)');
+
+contentBlocks[2].style.display = 'none';
+contentBlocks[3].style.display = 'none';
+
+const mediaQueryTablet = window.matchMedia('(min-width: 768px)');
+
+function handleTabletChange(evt) {
+  if (evt.matches) {
+    contentElement.style.display = 'block';
+  } else {
+    contentElement.style.display = 'none';
+  }
+}
+
+mediaQueryTablet.addListener(handleTabletChange);
+handleTabletChange(mediaQueryTablet);
+
+for (let i = 0; i < contentBlocks.length; i++) {
+
+  const hideContent = () => {
+    contentBlocks[2].style.display = 'none';
+    contentBlocks[3].style.display = 'none';
+    button.textContent = 'Подробнее';
+  };
+
+  const showContent = () => {
+    contentBlocks[2].style.display = 'block';
+    contentBlocks[3].style.display = 'block';
+    button.textContent = 'Cвернуть';
+  };
+
+
+  button.addEventListener('click', ()=> {
+
+    if (mediaQueryMobile.matches) {
+      if (contentBlocks[i].classList.contains('hidden-mobile')) {
+        contentBlocks[i].classList.remove('hidden-mobile');
+        showContent();
+        contentElement.style.display = 'block';
+      } else {
+        hideContent();
+        contentBlocks[i].classList.add('hidden-mobile');
+        contentElement.style.display = 'none';
+      }
+    }
+
+
+    if (mediaQueryTablet.matches) {
+
+      if (contentBlocks[i].classList.contains('hidden-tablet')) {
+        showContent();
+        contentBlocks[i].classList.remove('hidden-tablet');
+      } else {
+        hideContent();
+        contentBlocks[i].classList.add('hidden-tablet');
+      }
+    }
+  });
+}
+
+
+// Аккордеон в футере
+
 const buttons = document.querySelectorAll('.page-footer__button');
 const wrappers = document.querySelectorAll('.page-footer__wrapper-block');
 
