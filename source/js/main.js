2adfe59d9eeb9a9fc58777db8644ac3b5bd0176f
Yulia Asfanderova <nadberejenka@yandex.ru>
2022-09-19 18:08:33 +0300

    form

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,9 @@
 // Кнопка скрытия/показа контента в разделе "О компании"
 
-const button = document.querySelector('.about button');
+const buttonAbout = document.querySelector('.about button');
 const contentBlocks = document.querySelectorAll('.about p');
 const contentElement = document.querySelector('.about span');
 
-// const mediaQueryTablet = window.matchMedia('(min-width: 768px)');
 const mediaQueryMobile = window.matchMedia('(max-width: 767.9px)');
 
 contentBlocks[2].style.display = 'none';
@@ -28,17 +27,17 @@ for (let i = 0; i < contentBlocks.length; i++) {
   const hideContent = () => {
     contentBlocks[2].style.display = 'none';
     contentBlocks[3].style.display = 'none';
-    button.textContent = 'Подробнее';
+    buttonAbout.textContent = 'Подробнее';
   };
 
   const showContent = () => {
     contentBlocks[2].style.display = 'block';
     contentBlocks[3].style.display = 'block';
-    button.textContent = 'Cвернуть';
+    buttonAbout.textContent = 'Cвернуть';
   };
 
 
-  button.addEventListener('click', ()=> {
+  buttonAbout.addEventListener('click', ()=> {
 
     if (mediaQueryMobile.matches) {
       if (contentBlocks[i].classList.contains('hidden-mobile')) {
@@ -106,4 +105,63 @@ for (let i = 0; i < buttons.length; i++) {
   });
 }
 
+// Маска телефона
+
+window.addEventListener('DOMContentLoaded', () => {
+  [].forEach.call(document.querySelectorAll('#phone'), (input) => {
+    let keyCode;
+    function Mask(event) {
+      let pos = this.selectionStart;
+      if (pos < 3) {
+        event.preventDefault();
+      }
+      let matrix = '+7(___)_______';
+      let i = 0;
+      let def = matrix.replace(/\D/g, '');
+      let val = this.value.replace(/\D/g, '');
+      let newValue = matrix.replace(/[_\d]/g, function (a) {
+        return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+      });
+      i = newValue.indexOf('_');
+      if (i !== -1) {
+        newValue = newValue.slice(0, i);
+      }
+      let reg = matrix.substr(0, this.value.length).replace(/_+/g,
+          function (a) {
+            return '\\d{1,' + a.length + '}';
+          }).replace(/[+()]/g, '\\$&');
+      reg = new RegExp('^' + reg + '$');
+      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+        this.value = newValue;
+      }
+      if (event.type === 'blur' && this.value.length < 5) {
+        this.value = '';
+      }
+    }
+    input.addEventListener('input', Mask, false);
+    input.addEventListener('focus', Mask, false);
+    input.addEventListener('blur', Mask, false);
+    input.addEventListener('keydown', Mask, false);
+  });
+});
+
+const submitButton = document.querySelector('.feedback-form button');
+const inputs = document.querySelectorAll('.feedback-form input');
+const form = document.querySelector('.feedback-form');
+
+  form.addEventListener('submit', (evt)=> {
+
+    })
+
+
+
+
+
+
+
+// Local Storage
+const formData = document.querySelectorAll('input');
+for (let i = 0; i <= formData.length - 1; i++) {
+  localStorage.setItem('formData', formData.value);
+}
 
